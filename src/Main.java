446d6161f5e650ba9196bcf79dfa34930a247984
Vanya Iarovitzine <ivphoneac@gmail.com>
2023-01-12 12:59:04 +0300

    shapes added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Rectangle {
+    private double length;
+    private double width;
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double getLength() {
+        return length;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public double getArea() {
+        return length * width;
+    }
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Circle {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+    public double getSide1() {
+        return side1;
+    }
+    public double getSide2() {
+        return side2;
+    }
+    public double getSide3() {
+        return side3;
+    }
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+    public double getPerimeter() {
+        return side1 + side2 + side3;
     }
 }
